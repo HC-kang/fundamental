fbf3a319d7af4226c142db8a2b7375b6049ed7d0
HC-kang <weston0713@gmail.com>
2021-05-14 17:51:35 +0900

    0514 수업끝

--- fundamental_05.py
+++ fundamental_05.py
@@ -243,3 +243,114 @@ for n_sample in n_samples:
 # K-means와 DBSCAN의 소요 시간 그래프화
 
 fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(x, kmeans_time, c = 'red', marker = 'x', label = 'K-means elapsed time')
+ax.scatter(x, dbscan_time, c = 'green', label = 'DBSCAN elapsed time')
+ax.set_xlabel('# of samples')
+ax.set_ylabel('time(s)')
+ax.legend()
+ax.grid()
+
+
+
+
+#####
+# 차원축소(1) - PCA
+###
+# 1. 데이터 개요
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+        # 차원축소 예제 - 유방암 데이터셋
+
+cancer = load_breast_cancer()
+        # 데이터 불러오기
+
+        # y = 0(Malignant - 악성 종양), y = 1(Benign - 양성 종양)
+cancer_X, cancer_y = cancer.data, cancer['target']
+train_X , test_X, train_y, test_y = train_test_split(cancer_X, cancer_y, test_size = 0.1, random_state = 10)
+        # 데이터 분할
+print('전체 검사자 수 : {}'.format(len(cancer_X)))
+print('Train dataset에 사용되는 검사자 수 : {}'.format(len(train_X)))
+print('Test Dataset에 사용되는 검사자 수 : {}'.format(len(test_X)))
+cancer_df = pd.DataFrame(cancer_X, columns = cancer['feature_names'])
+cancer_df.head()
+cancer_df.info()
+cancer_df.describe()
+
+# 2. 유방암 데이터셋에 PCA 알고리즘 적용 예제
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from collections import Counter
+
+# color dict
+color_dict = {0:'red', 1:'blue', 2:'red', 3:'blue'}
+target_dict = {0:'marlignant_train', 1:'benign_train', 2:'mailgnant_test', 3:'benign_test'}
+
+# Train data에 PCA 알고리즘 적용
+train_X_ = StandardScaler().fit_transform(train_X)
+        # 불러온 데이터에 대한 정규화 -> 각 column의 range of value가 전부 다르기에 정규화 진행
+train_df = pd.DataFrame(train_X_, columns = cancer['feature_names'])
+pca = PCA(n_components=2)
+        # 주성분 수 2개, 즉 기저 방향벡터를 2개로 하는 PCA 알고리즘 시행
+pc = pca.fit_transform(train_df)
+        # 정규화 하는 이유 : 예를 들어, mean radius와 mean texture의 값이 같다면 
+        # 같은 영향력을 끼칠텐데 두가지의 범위가 다름. 즉 중요도가 왜곡될 수 있으므로 0~1사이로 정규화 실시.
+
+# Test data에 PCA 알고리즘 적용
+test_X = StandardScaler().fit_transform(test_X) 
+test_df = pd.DataFrame(test_X, columns = cancer['feature_names'])
+pca_test = PCA(n_components = 2)
+pc_test = pca_test.fit_transform(test_df)
+
+
+# 훈련한 classifier의 decision boundary를 그리는 함수
+def plot_decision_boundary(X, clf, ax):
+    h = .02 # step size in the mesh
+    # create a mesh to plot in
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    ax.contour(xx, yy, Z, cmap = 'Blues')
+
+# PCA를 적용한 train dat의 classifier 훈련 : classifier 로 SVM을 사용한다는 정도만 알아둡시다.
+clf = svm.SVC(kernel = 'rbf', gamma = 0.5, C = 0.8) # 여기서는 classifier로 SVM을 사용한다
+clf.fit(pc, train_y) # train data로 classifier훈련
+
+# PCA를 적용하지 않은 original data의 SVM 훈련
+clf_orig = svm.SVC(kernel = 'rbf', gamma = 0.5, C = 0.8)
+clf_orig.fit(train_df, train_y)
+
+
+# 캔버스 도식
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+# malignant와 benign의 SVM decision boundary 그리기
+plot_decision_boundary(pc, clf, ax)
+
+# Train data 도식
+for cluster in range(2):
+    sub_cancer_points = pc[train_y == cluster]
+    ax.scatter(sub_cancer_points[:,0], sub_cancer_points[:, 1], edgecolor = color_dict[cluster], c = 'none', label = target_dict[cluster])
+# Test data 도식
+for cluster in range(2):
+    sub_cancer_points = pc_test[test_y == cluster ]
+    ax.scatter(sub_cancer_points[:, 0], sub_cancer_points[:, 1], marker = 'x', c = color_dict[cluster+2], label = target_dict[cluster+2])
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_title('PCA-Breast cancer dataset')
+ax.legend()
+ax.grid()
+
+# Scoring
+pca_test_accuracy_dict = Counter(clf.predict(pc_test) == test_y)
+pca_test_accuracy_dict
+orig_test_accuracy_dict = Counter(clf_orig.predict(test_df) == test_y)
+orig_test_accuracy_dict
+
+print("PCA 분석을 사용한 Test dataset accuracy: {}명/{}명 => {:.3f}".format(pca_test_accuracy_dict[True], sum(pca_test_accuracy_dict.value)))
+print("PCA를 적용하지 않은 Test dataset accuracy: {}명/{}명 => {:.3f}".format(orig_test_accuracy_dict[True], sum(orig_test_accuracy_dict.value
